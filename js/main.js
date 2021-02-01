da10fcc1f93a605d024cb2c6d46b02a43e4ae812
natalia_levko <54891962+NataliaLevko@users.noreply.github.com>
2021-02-01 16:27:23 +0300

    Добавляет две функции поиска чисел из диапазона

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//Функция, возвращающая случайное целое число из переданного диапазона включительно
+const randomIntNumber = function (min, max) {
+if (min >= 0 && max > min) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+if(max <= min || min < 0) {
+  console.log('Задайте корректный диапазон');
+  }
+}
+
+//Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+const randomFloatNumber = function (minNumber, maxNumber, floatValue) {
+  let totalNumber = 0;
+
+  if (minNumber >= 0 && maxNumber > minNumber) {
+    totalNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+    return totalNumber = totalNumber.toFixed(floatValue);
+  }
+  if(maxNumber <= minNumber || minNumber < 0 ) {
+    console.log('Задайте корректный диапазон');
+    }
+  }
+  /*
+  Во втором задании нет уточнения относительно значения параметра floatValue (количество_знаков_после_запятой).
+  Сейчас по умолчанию в этом параметре может также передаваться дробное значение, что не очень правильно, как я считаю.
+  Но в задании нет пояснения, какой должен быть результат, если пользователь передает аргумент floatValue дробный.
+  */
